dfc84b9bb0031295ac6df191187045edbd277a33
eRRoR109 <fominyh2.vv@edu.spbstu.ru>
2024-05-09 23:55:38 +0300

    add filter panel

--- src/App.js
+++ src/App.js
@@ -1,30 +1,49 @@
 import React, { Component } from 'react'
-
 import { initialState, StoreContext } from './utils/store'
 import RecyclePoints from './components/RecyclePoints'
-import ProjectDescription from './components/ProjectDescription'
-
+import SidePanel from './components/SidePanel'
 import './styles/index.sass'
 
+const   recycleTypes = {
+    plastic: [0, 5, 6, 8, 90, 95, 96, 98],
+    paper: [1, 5, 7, 8, 91, 95, 97, 98],
+    rare: [3, 93],
+    glass: [2, 6, 7, 8, 92, 96, 97, 98],
+    battery: [4]
+}
 
 class App extends Component {
-  constructor(props) {
-    super(props)
+    constructor(props) {
+        super(props)
+        this.state = initialState({
+            state: this.state,
+            setState: this.setState,
+        })
+        this.state.choosenFilters = [];
+    }
 
-    this.state = initialState({
-      state: this.state,
-      setState: this.setState,
-    })
-  }
+    onFilterChange = (value) => {
+        const updatedFilters = [...this.state.choosenFilters];
+        const typeList = recycleTypes[value];
 
-  render = () => (
-    <StoreContext.Provider value={this.state}>
-      <div className="App">
-        <ProjectDescription />
-        <RecyclePoints />
-      </div>
-    </StoreContext.Provider>
-  )
+        const index = updatedFilters.findIndex(filter => filter === typeList);
+        if (index !== -1) {
+            updatedFilters.splice(index, 1);
+        } else {
+            updatedFilters.push(typeList);
+        }
+
+        this.setState({ choosenFilters: updatedFilters });
+    };
+
+    render = () => (
+        <StoreContext.Provider value={this.state}>
+            <div className="App">
+                <SidePanel onFilterChange={this.onFilterChange} />
+                <RecyclePoints choosenFilters={this.state.choosenFilters.flat()} />
+            </div>
+        </StoreContext.Provider>
+    )
 }
 
 export default App
---
+++ src/components/Filter.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import IconPlastic from '../img/filter_images/90.png'
+import IconPaper from '../img/filter_images/91.png'
+import IconGlass from '../img/filter_images/92.png'
+import IconRare from '../img/filter_images/93.png'
+import IconBat from '../img/filter_images/4.png'
+
+function Filter({ onFilterChange }) {
+    const handleCheckboxChange = (event) => {
+        const filterType = event.target.value;
+        onFilterChange(filterType);
+    };
+
+    return (
+        <div className="filter">
+        <div className="filter__container">
+            <label className="filter__checkbox">
+                <input type="checkbox" value="plastic" onChange={handleCheckboxChange} /> Пластик
+                <img className="filter__img" src={IconPlastic} alt="1" />
+            </label>
+            <label className="filter__checkbox">
+                <input type="checkbox" value="paper" onChange={handleCheckboxChange} /> Бумага
+                <img className="filter__img" src={IconPaper} alt="2" />
+            </label>
+            <label className="filter__checkbox">
+                <input type="checkbox" value="glass" onChange={handleCheckboxChange} /> Стекло
+                <img className="filter__img" src={IconGlass} alt="3" />
+            </label>
+            <label className="filter__checkbox">
+                <input type="checkbox" value="rare" onChange={handleCheckboxChange} /> Редкости
+                <img className="filter__img" src={IconRare} alt="4" />
+            </label>
+            <label className="filter__checkbox">
+                <input type="checkbox" value="battery" onChange={handleCheckboxChange} /> Батарейки
+                <img className="filter__img" src={IconBat} alt="5" />
+            </label>
+            </div>
+        </div>
+    );
+}
+
+export default Filter;
